4d922b35cd4337e323e1c78579b47198c9cd0330
Lisen Hatson <139343304+LisenHatson@users.noreply.github.com>
2023-09-28 21:03:54 +0700

    Update config.h

--- config.h
+++ config.h
@@ -14,10 +14,10 @@ static const Block blocks[] = {
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
+	/* {"",	"sb-doppler",	0,	13}, */
+	/* {"",	"sb-forecast",	18000,	5},  */
+	/* {"",	"sb-mailbox",	180,	12}, */
+	/* {"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	1,	1},
