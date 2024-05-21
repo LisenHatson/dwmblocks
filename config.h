a515b0460c9462630858d73505cc056a7da3e760
Lisen Hatson <139343304+LisenHatson@users.noreply.github.com>
2024-05-21 16:33:30 +0700

    my

--- config.h
+++ config.h
@@ -4,13 +4,9 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
-	{"",	"sb-music",	0,	11}, 
+
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
 	/* {"",	"sb-memory",	10,	14}, */
 	/* {"",	"sb-cpu",		10,	18}, */
@@ -20,6 +16,7 @@ static const Block blocks[] = {
 	/* {"",	"sb-mailbox",	180,	12}, */
 	/* {"",	"sb-nettraf",	1,	16}, */
 	{"",	"sb-volume",	0,	10},
+	{"",	"sb-music",	0,	11},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	1,	1},
 	{"",	"sb-internet",	5,	4},
